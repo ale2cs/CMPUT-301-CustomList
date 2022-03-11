6e5857fe0bd1bc3280a10343e4402afb5cb76961
ale2cs <ale2@ualberta.ca>
2022-03-10 21:19:06 -0700

    Added deleteCity method to CustomList and deleteCityTest to CustomListTest

--- app/src/test/java/CustomListTest.java
+++ app/src/test/java/CustomListTest.java
@@ -1,4 +1,5 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import com.example.simpleparadox.listycity.City;
 import com.example.simpleparadox.listycity.CustomList;
@@ -23,6 +24,18 @@ public class CustomListTest {
             list.addCity(new City("Halifax", "NS"));
             assertEquals(listSize+1, list.getCount());
 
+        }
+
+        @Test
+        public void deleteCityTest(){
+            int listSize = list.getCount();
+
+            City city = new City("Halifax", "NS");
+            list.addCity(city);
+            assertEquals(listSize+1, list.getCount());
+
+            list.deleteCity(city);
+            assertEquals(listSize, list.getCount());
         }
 
 }
